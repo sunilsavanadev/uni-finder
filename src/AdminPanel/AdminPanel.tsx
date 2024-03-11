0fa782a2e6689cffc83dee3d6753d2d4acb02c61
ANORA\sunilkumar <sunilkumar@anorasolutions.com>
2024-03-11 17:46:27 +0530

    added function for both university and course

--- src/AdminPanel/AdminPanel.tsx
+++ src/AdminPanel/AdminPanel.tsx
@@ -1,95 +1,164 @@
-import React, {useEffect, useState } from 'react'
-import "./AdminPanel.css"
-import {getuniversities, getuniversitydata, setDocs } from './AdminPanelEvents'
+import { useState } from "react"
+import"./AdminPanel.css"
+import { AddCourse, AddUniversity, UniversityData } from "./AdminPanelEvents"
 function AdminPanel () {
-    const[formdata,setformdata]=useState<any>({})
-    const[universities,setUniversities]=useState<any>([])
-    const[data,setData]=useState<any>({})
+  const [state,setState]=useState("university")
+  const [data,setData]=useState({
+    name:"",
+    ranking:"",
+    tution:"",
+    living:"",
+    link:"",
+    about:""
+  })
+  const [courseData, setCourseData] = useState({
+    course_id: '',
+    course_name: '',
+    level: '',
+    duration: '',
+    academic_criteria: '',
+    additional_requirements: '',
+    fees: '',
+    intake: '',
+    description: '',
+    career_opportunities: ''
+  });
+// fetching university data
+UniversityData().then((val:any)=>{
+  val.forEach((doc:any) => {
+    console.log(doc.id,"=>",doc.data())
+  })})
 
-// On click form submit updating the university list to the database.
-    const formsubmit=(e:any)=>{
-        e.preventDefault()
-        setDocs(formdata,formdata["name"]).then(()=>{
-            window.alert("university added")
-        })
+//onclick function of university button
+  const OnclickUniversity=()=>{
+    setState("university")
+  }
+//onclick function for course button
+  const OnclickCourse =()=>{
+    setState("course")
+  }
+//onclick function for exam button
+    const OnclickExam =()=>{
+      setState("exam")
     }
-// Function for getting all the require infor mation from the form.
-    const handleformdata=(e:any)=>{
-      e.preventDefault()
-        setformdata({
-            ...formdata,
-            [e.target.name]:e.target.value
-        })
-    }
+//onclick function for eligibility  button
+  const OnclickEligibility =()=>{
+    setState("eligibility")
+  }
+// onformsubmit function
+const OnFormsubmit=async(e:any)=>{
+  e.preventDefault()
+ await AddUniversity()
+ setData({
+  name:"",
+  ranking:"",
+  tution:"",
+  living:"",
+  link:"",
+  about:""
+})
+}
+//HandleChange function
+const HandleChange=(e:any)=>{
+  setData(()=>({
+    ...data,
+    [e.target.name]:e.target.value
+  }))
+}
+const handleChange = (e:any) => {
+  const { name, value } = e.target;
+  setCourseData({
+    ...courseData,
+    [name]: value
+  });
+};
 
-    async function renderFunction() {
-        var result = await getuniversities()
-        result.forEach((res)=>{ return universities.includes(res.id)?"": setUniversities([...universities,res.id])})
-    }
-    renderFunction()  
-    const onclickUniversity=async(value:string)=>{
-      await getuniversitydata(value).then((result:any)=>{
-          console.log(result.data())
-       })
-    }
+const handleSubmit = (e:any) => {
+  e.preventDefault();
+  console.log(courseData); // Do whatever you want with the form data
+};
   return (
-    <div className='pannel-container'>
-        <div className='universities'>
-            {universities.map((value:any)=>{
-                return(
-                    <div onClick={()=>onclickUniversity(value)}>{value}</div>
-                )
-            })}
-        </div>
-        <form className='form' onSubmit={formsubmit}>
-            <div className='form-content'>
-                <label htmlFor="name">University Name:</label>
-                <input id='name' type="text" name="name" value={data.name} required onChange={handleformdata} />
-            </div>
-            <div className='form-content'>
-                <label htmlFor="rank">University Ranking(QS) :</label>
-                <input id="rank"type="text" name="QSranking" value={data.QSranking} required onChange={handleformdata} />
-            </div>
-            <div className='form-content'>
-                <label htmlFor="avg_tution">Avg_tution:</label>
-                <input id='avg_tution' type="text" name="avg_tution" value={data.avg_tution} required onChange={handleformdata} />
-            </div>
-            <div className='form-content'>
-                <label htmlFor="avg_livingcost">Avg_livingcost:</label>
-                <input id='avg_livingcost' type="text" name="avg_livingcost" value={data.avg_livingcost}  required onChange={handleformdata} />
-            </div>
-            <div className='form-content-exams'>
-              <div className='exams'>
-                <label htmlFor="gre">GRE:</label>
-                <input id='gre' type="text" name="gre" value={data.gre}  onChange={handleformdata} />
-              </div>
-              <div className='exams'>
-                <label htmlFor="toefl">TOEFL:</label>
-                <input id='toefl' type="text" name="toefl" value={data.toefl}  onChange={handleformdata} />
-              </div>
-              <div className='exams'>
-                <label htmlFor="dulingo">DULINGO:</label>
-                <input id='dulingo' type="text" name="dulingo" value={data.dulingo}  onChange={handleformdata} />
-              </div>
-              <div className='exams'>
-                <label htmlFor="ielts">IELTS:</label>
-                <input id='ielts' type="text" name="ielts" value={data.ielts} onChange={handleformdata} />
-              </div>
-              <div className='exams'>
-                <label htmlFor="sat">SAT:</label>
-                <input id='sat' type="text" name="sat" value={data.sat} onChange={handleformdata} />
-              </div>
-              <div className='exams'>
-                <label htmlFor="gmat">GMAT:</label>
-                <input id='gmat' type="text" name="gmat" value={data.gmat} onChange={handleformdata} />
-              </div>
-            </div>
-            <div className='about'>
-                <label  htmlFor="about">About:</label>
-                <textarea id='abput' name="about" value={data.about} onChange={handleformdata} />
-            </div>  
-            <input className='submit' type="submit" />
+    <div className="container">
+      <div className="section-buttons">
+        <button onClick={OnclickUniversity} style={{background:state==="university"?"#3d5ee1":"",color:state==="university"?"white":"#6f6f6f"}}>University</button>
+        <button onClick={OnclickCourse} style={{background:state==="course"?"#3d5ee1":"",color:state==="course"?"white":"#6f6f6f"}}>Courses</button>
+        <button onClick={OnclickExam} style={{background:state==="exam"?"#3d5ee1":"",color:state==="exam"?"white":"#6f6f6f"}}>Exams</button>
+        <button onClick={OnclickEligibility} style={{background:state==="eligibility"?"#3d5ee1":"",color:state==="eligibility"?"white":"#6f6f6f"}}>Eligibily</button>
+      </div>
+      {state==="university"&&
+      <div className="form-container">
+        <form className="form" onSubmit={OnFormsubmit}>
+          <div className="form-input">
+            <label>Name</label>
+            <input name="name" value={data.name} required onChange={HandleChange}/>
+          </div>
+          <div className="form-input" >
+            <label>Ranking</label>
+            <input name="ranking" value={data.ranking} required onChange={HandleChange}/>
+          </div>
+          <div className="form-input">
+            <label>Avg Tution</label>
+            <input name="tution" value={data.tution} required onChange={HandleChange}/>
+          </div>
+          <div className="form-input">
+            <label>Avg Living</label>
+            <input name="living" value={data.living} required onChange={HandleChange}/>
+          </div>
+          <div className="form-input">
+            <label>university link</label>
+            <input name="link" value={data.link} required onChange={HandleChange}/>
+          </div>
+          <div className="form-input">
+            <label>About</label>
+            <input name="about" value={data.about} required onChange={HandleChange}/>
+          </div>
+          <input type="submit" value="submit" onClick={()=>{AddUniversity()}}/>
         </form>
+        <form onSubmit={handleSubmit}>
+      <label>
+        Course ID:
+        <input type="text" name="course_id" value={courseData.course_id} onChange={handleChange} />
+      </label>
+      <label>
+        Course Name:
+        <input type="text" name="course_name" value={courseData.course_name} onChange={handleChange} />
+      </label>
+      <label>
+        Level:
+        <input type="text" name="level" value={courseData.level} onChange={handleChange} />
+      </label>
+      <label>
+        Duration:
+        <input type="text" name="duration" value={courseData.duration} onChange={handleChange} />
+      </label>
+      <label>
+        Academic Criteria:
+        <input type="text" name="academic_criteria" value={courseData.academic_criteria} onChange={handleChange} />
+      </label>
+      <label>
+        Additional Requirements:
+        <input type="text" name="additional_requirements" value={courseData.additional_requirements} onChange={handleChange} />
+      </label>
+      <label>
+        Fees:
+        <input type="text" name="fees" value={courseData.fees} onChange={handleChange} />
+      </label>
+      <label>
+        Intake:
+        <input type="text" name="intake" value={courseData.intake} onChange={handleChange} />
+      </label>
+      <label>
+        Description:
+        <input type="text" name="description" value={courseData.description} onChange={handleChange} />
+      </label>
+      <label>
+        Career Opportunities:
+        <input type="text" name="career_opportunities" value={courseData.career_opportunities} onChange={handleChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+      </div>}
+      
     </div>
   )
 }
--- src/AdminPanel/AdminPanelEvents.tsx
+++ src/AdminPanel/AdminPanelEvents.tsx
@@ -1,20 +1,57 @@
-import { collection, addDoc, setDoc, doc, getDoc, getDocs, getDocFromCache } from "firebase/firestore"; 
+import React from "react";
+import { collection, doc, getDocs, setDoc } from "firebase/firestore"; 
 import { db } from "../firebase";
 
 
-const setDocs =async(data:any,name:string)=>{
-    await setDoc(doc(db, "universities", name),data)
-    console.log(data)
+const universityData = {
+  university_id: 1,
+  university_name: "Example University",
+  country: "India",
+  website_url: "https://exampleuniversity.edu",
+  description: "Example University is a prestigious institution offering a wide range of programs.",
+  logo_url: "https://exampleuniversity.edu/logo.png",
+  contact_information: {
+    address: "123 Example St, City, State, Country",
+    phone: "+91-1234567890",
+    email: "info@exampleuniversity.edu"
+  },
+  social_media_links: {
+    facebook: "https://www.facebook.com/exampleuniversity",
+    twitter: "https://twitter.com/exampleuniversity",
+    instagram: "https://www.instagram.com/exampleuniversity"
+  }
+};
+
+const coursesData = 
+  {
+    course_id:1,
+    university_id:"1",
+    course_name: "Bachelor of Engineering",
+    course_discription:"good course",
+    tution:"$15,000",
+    credits:"30",
+    level: "Undergraduate",
+    duration: "4 years",
+    Toefl:"85",
+    Gre:"310",
+    Sat:"650",
+    Ielts:"6.5",
+    Dulingo:"100",
+  }
+// Add a new document in collection "universities"
+const AddUniversity= async () => {
+  const DocRef = doc(db, "Universities",universityData.university_name);
+  await setDoc(DocRef,universityData);
 }
-const getuniversities =async()=>{
-    const docRef = getDocs(collection(db, "universities"));
-    var result =await docRef
-    return result
+const AddCourse= async () => {
+  const DocRef = doc(db, "courses",coursesData.course_name);
+  await setDoc(DocRef,coursesData);
+}
+// getting information from universities
+const UniversityData = async()=>{
+  const querySnapshot = await getDocs(collection(db, "Universities"));
+  return querySnapshot
+
 }
 
-const getuniversitydata=async(university:string)=>{
-    const docRef = doc(db, "universities",university);
-    const docSnap = await getDoc(docRef);
-    return docSnap
-}
-export {setDocs,getuniversities,getuniversitydata}
+export {AddUniversity,UniversityData,AddCourse}
